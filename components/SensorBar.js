b7860badcab019553e8e6b8f479329544c2f8485
Quinn Casey <qcasey1@gmail.com>
2019-10-21 14:46:09 -0700

    remove right hand alignment

--- components/SensorBar.js
+++ components/SensorBar.js
@@ -51,36 +51,19 @@ export default class SensorsBar extends React.Component {
 		redlineLength = 10;
 		fillPercent = (this.props.fill > 100) ? "100%" : Math.round(this.props.fill)+"%"; // correct any extreme values
 
-		if(this.props.align == "Right") {
-			return (
-				<View style={{flexDirection: 'row', flex: 1, paddingBottom: 20}}>
-					<View style={{flexDirection: 'column', flex: 1}}>
-						<View style={styles.barTextContainer}>
-							<Text style={styles.barText}>{this.props.title}</Text>
-							<Text style={styles.barTextValue}>{this.props.val}</Text>
-						</View>
-						<View style={[styles.barOuterContainer, {height: Number.parseInt(this.props.barHeight, 10)}]}>
-							<View style={{width: 100-redlineLength+"%"}}><View style={{height: Number.parseInt(this.props.barHeight, 10)-4, width: fillPercent, backgroundColor: "#FF5722"}}></View></View>
-							<View style={{ height: Number.parseInt(this.props.barHeight, 10)-4, width: redlineLength+"%",  backgroundColor: "#FB334C"}}></View>
-						</View>
+		return (
+			<View style={{flexDirection: 'row', flex: 1, paddingBottom: 20}}>
+				<View style={{flexDirection: 'column', flex: 1}}>
+					<View style={styles.barTextContainer}>
+						<Text style={styles.barText}>{this.props.title}</Text>
+						<Text style={styles.barTextValue}>{this.props.val}</Text>
+					</View>
+					<View style={[styles.barOuterContainer, {height: Number.parseInt(this.props.barHeight, 10)}]}>
+						<View style={{width: 100-redlineLength+"%"}}><View style={{height: Number.parseInt(this.props.barHeight, 10)-4, width: fillPercent, backgroundColor: "#FF5722"}}></View></View>
+						<View style={{ height: Number.parseInt(this.props.barHeight, 10)-4, width: redlineLength+"%",  backgroundColor: "#FB334C"}}></View>
 					</View>
 				</View>
-			);
-		} else {
-			return (
-				<View style={{flexDirection: 'row', flex: 1, paddingBottom: 20}}>
-					<View style={{flexDirection: 'column', flex: 1}}>
-						<View style={styles.barTextContainer}>
-							<Text style={styles.barText}>{this.props.title}</Text>
-							<Text style={styles.barTextValue}>{this.props.val}</Text>
-						</View>
-						<View style={[styles.barOuterContainer, {height: Number.parseInt(this.props.barHeight, 10)}]}>
-							<View style={{ height: Number.parseInt(this.props.barHeight, 10)-4, width: redlineLength+"%",  backgroundColor: "#FB334C"}}></View>
-							<View style={{width: 100-redlineLength+"%"}}><View style={{height: Number.parseInt(this.props.barHeight, 10)-4, width: fillPercent, backgroundColor: "#FF5722"}}></View></View>
-						</View>
-					</View>
-				</View>
-			);
-		}
+			</View>
+		);
 	}
 }
